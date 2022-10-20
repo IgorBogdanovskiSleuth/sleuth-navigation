4be31bf5fd59f12fca43748a2e5e4e3c04a1655c
IgorBogdanovskiPre <ibogdanovski@sleuth.io>
2022-10-20 20:29:37 +0100

    Added the primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,24 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     fontFamily: {
-      'nunito': ['Open Sans', 'sans-serif'],
+      'nunito': ['"Open Sans"', 'sans-serif'],
       'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
     },
     colors: {
-      'sleuth': '#236A91'
+      primary: {
+        25: '#FBFCFE',
+        50: '#F5F8FC',
+        100: '#EBF2F8',
+        150: '#66afe9',
+        200: '#D6E4F1',
+        300: '#C2D7E9',
+        400: '#99BDDB',
+        500: '#70A2CD',
+        600: '#5C95C5',
+        700: '#337AB7',
+        800: '#296292',
+        900: '#1F496E', 
+      }
     },
 
     extend: {},
