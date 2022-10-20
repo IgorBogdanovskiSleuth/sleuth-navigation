4bf42449e63fb55705965cc185004a823c52460d
IgorBogdanovskiPre <ibogdanovski@sleuth.io>
2022-10-20 20:35:48 +0100

    added Open Sans font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     fontFamily: {
-      'nunito': ['"Open Sans"', 'sans-serif'],
-      'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+      'Open Sans': ['Open Sans', 'sans-serif'],
+      'MyFont': ['"Open Sans"', 'sans-serif'] // Ensure fonts with spaces have " " surrounding it.
     },
     colors: {
       primary: {
